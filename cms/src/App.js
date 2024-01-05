b38458060f8f2ad5f6d03f8438cd447e394e7113
Ben Vilakazi <xolanithengubane@gmail.com>
2024-01-05 22:45:48 +0200

    cms

---
+++ cms/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="CMS-master">
+    <nav className="navbar app">App bar</nav>
+    <nav className="navbar board">App board </nav>
+    <div className="board-columns">
+      board column
+    </div>
+    </div>
+  );
+}
+
+export default App;
