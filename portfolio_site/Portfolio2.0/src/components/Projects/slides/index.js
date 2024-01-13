383f6dfe4bbadd16aef695052b386adfab3eb15e
Ben Vilakazi <xolanithengubane@gmail.com>
2024-01-13 22:26:52 +0200

    buck

---
+++ portfolio_site/Portfolio2.0/src/components/Projects/slides/index.js
@@ -0,0 +1,135 @@
+import tiktok from "./images/tiktok.png";
+import youtube from "./images/yt.png";
+import coc from "./images/coc.png";
+import weather from "./images/weather.png";
+import sprite from "./images/sprite.png";
+import Realtor from "./images/Realtor.png";
+import drumkit from "./images/drumkit.png";
+import advice from "./images/advice.png";
+import bookshelf from "./images/bookshelf.png";
+import cal from "./images/calculator.png";
+import emo from "./images/emo.png";
+import memory from "./images/memory.png";
+import todo from "./images/todo.png";
+import we from "./images/we.png";
+
+const slides = [
+  {
+    imgSrc: tiktok,
+    name: "TikTok Clone",
+    description: "A FullStack TikTok clone created using React,Stripe and Next...",
+    code: "https://github.com/codexblue/TikTok-clone",
+    demo: "https://tiktok-clone-zhy.vercel.app/"
+  },
+  {
+    imgSrc: youtube,
+    name: "YouTube Clone",
+    description:
+      "a Modern YouTube Clone Application in React JS with Material UI 5",
+    code: "https://github.com/codexblue/YT-clone",
+    demo: "https://youtubeclone-farhan.netlify.app/"
+  },
+  {
+    imgSrc: coc,
+    name: "cards of carousal",
+    description:
+      'A clone of the card game "Cards Against Humanity". Created using Express.js, WebSocket and React.',
+    code: "https://github.com/codexblue/coc",
+    demo: "https://www.cardsofcarousal.com/"
+  },
+  {
+    imgSrc: sprite,
+    name: "spritesheet-animate",
+    description:
+      "A spritesheet animator created using JavaScript. Scroll down to see it in action.",
+    code: "https://github.com/codexblue/js-spritesheet-animators",
+    demo: "https://spritesheetanimator.netlify.app"
+  },
+  {
+    imgSrc: weather,
+    name: "weatherToday",
+    description:
+      "A weather app created using simple Html, CSS, and JavaScript.",
+    code: "https://github.com/codexblue/react-weather-app",
+    demo: "https://weatherappben.netlify.app/"
+  },
+  {
+    imgSrc: Realtor,
+    name: "Realtor",
+    description:
+      "a Real Estate Application built with React & Next.js",
+    code: "https://github.com/BenVilakazi/Real-Estate",
+    demo: "https://realtor.vercel.app/"
+  },
+  {
+    imgSrc: drumkit,
+    name: "Drumkit",
+    description:
+      "A drumkit app in JavaScript is a software application that allows users to play drums on their computer. The app typically includes a variety of drum sounds, as well as a user interface that allows users to control the drums. Drumkit apps can be used for practicing drumming, composing music, or simply having fun.",
+    code: "https://github.com/BenVilakazi/drum-mac",
+    demo: "https://drum-mac.vercel.app/"
+  },
+  {
+    imgSrc: memory,
+    name: "Memory Game",
+    description:
+      "Memory game in React is a card matching game where players try to match pairs of cards.",
+    code: "https://github.com/BenVilakazi/automatic-disco",
+    demo: "https://automatic-disco-omega.vercel.app/"
+  },
+  {
+    imgSrc: bookshelf,
+    name: "Book Library",
+    description:
+      "Online book library built in JavaScript, HTML and CSS",
+    code: "https://github.com/BenVilakazi/probable-disco",
+    demo: "https://probable-disco.vercel.app/"
+  },
+  {
+    imgSrc: cal,
+    name: "Calculator",
+    description:
+      "A simple calculator written in JavaScript that can perform basic arithmetic operations such as addition, subtraction, multiplication, and division.",
+    code: "https://github.com/BenVilakazi/silver-train",
+    demo: "https://silver-train.vercel.app/"
+  },
+  {
+    imgSrc: advice,
+    name: "Random Quote Generator",
+    description:
+      "A random quote generator built with React",
+    code: "https://github.com/BenVilakazi/quote-app",
+    demo: "https://dulcet-raindrop-5a5aea.netlify.app/"
+  },
+  {
+    imgSrc: we,
+    name: "WeatherApp",
+    description:
+      "A weather app in Js with geolocation tracker",
+    code: "https://github.com/BenVilakazi/weatherz",
+    demo: "https://gentle-semifreddo-0d8ba5.netlify.app/"
+  },
+  {
+    imgSrc: emo,
+    name: "Emotion Recognizer",
+    description:
+      "A Emotion Recognizer app built with TensorFlow.js",
+    code: "https://github.com/BenVilakazi/emo-rec",
+    demo: "https://radiant-biscochitos-1637f5.netlify.app/"
+  },
+  {
+    imgSrc: todo,
+    name: "To-Do list",
+    description:
+      "A Emotion Recognizer app built with TensorFlow.js",
+    code: "https://github.com/BenVilakazi/emo-rec",
+    demo: "https://earnest-trifle-10abc7.netlify.app/"
+  },
+
+
+
+
+
+
+];
+export default slides;
